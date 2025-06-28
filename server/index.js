0eaea722e85567daca9c7f7c8ae932be70ee82a9
TynaNguyen <giangnguyenpr96@gmail.com>
2025-06-28 23:09:41 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,96 +1,65 @@
-// /*
-// Copyright (c) 2017, ZOHO CORPORATION
-// License: MIT
-// */
-// const portfinder = require('portfinder');
-// var fs = require('fs');
-// var path = require('path');
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var errorHandler = require('errorhandler');
-// var morgan = require('morgan');
-// var serveIndex = require('serve-index');
-// var https = require('https');
-// var chalk = require('chalk');
+ZOHO.CREATOR.init().then(function (data) {
+  var queryParams = ZOHO.CREATOR.UTIL.getQueryParams();
+  console.log("queryParams:", queryParams);
+  var initparams = ZOHO.CREATOR.UTIL.getInitParams();
+  console.log("initparams:", initparams);
+  console.log("loginUser:", initparams.loginUser);
 
-// process.env.PWD = process.env.PWD || process.cwd();
+  function submitData() {
+    var appName = document.getElementById("appName").value;
+    var formName = document.getElementById("formName").value;
+    var jsonDataText = document.getElementById("jsonData").value;
+    console.log("jsonDataText", jsonDataText);
 
+    try {
+      var jsonData = JSON.parse(jsonDataText); // Parse the input JSON data
+      console.log("jsonData", jsonData);
 
-// var expressApp = express();
+      jsonData.forEach((record) => {
+        if (record.hasOwnProperty("data")) {
+          // Chuẩn bị formData chỉ khi record có key 'data'
+          const formData = { data: {} };
 
-// var portPromise = portfinder.getPortPromise({
-//   startPort: 5000,
-//   port: 5000,
-//   stopPort: 5009
-// });
+          // Lặp qua từng key trong đối tượng data của record
+          for (const key in record.data) {
+            if (record.data.hasOwnProperty(key)) {
+              // Thêm key và giá trị tương ứng vào formData
+              formData.data[key] = record.data[key];
+            }
+          }
 
-// portPromise.then((port) => {
-//   expressApp.set('port', port);
-//   expressApp.use(morgan('dev'));
-//   expressApp.use(bodyParser.json());
-//   expressApp.use(bodyParser.urlencoded({ extended: false }));
-//   expressApp.use(errorHandler());
+          console.log("formData", formData);
 
+          var config = {
+            appName: appName,
+            formName: formName,
+            data: formData.data, // Sử dụng trực tiếp dữ liệu trong formData
+          };
 
-//   expressApp.use('/', function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     let connectSrc = "";
-//     let manifest = fs.readFileSync(path.join(__dirname, "..", "plugin-manifest.json")).toString();
-//     manifest = JSON.parse(manifest);
-//     if (manifest != null && manifest.cspDomains != null && manifest.cspDomains["connect-src"] != null) {
-//       let connectDomains = manifest.cspDomains["connect-src"];
-//       if (validateDomains(connectDomains)) {
-//         console.log(chalk.bold.red(connectDomains + " - found to be invalid URL(s) in connect-src"));
-//         next();
-//         return false;
-//       }
-//       connectSrc = connectDomains.join(" ");
-//     }
-//     res.setHeader('Content-Security-Policy', 'connect-src https://*.zohostatic.com https://*.sigmausercontent.com ' + connectSrc);
-//     next();
-//   });
+          console.log("config===>", config);
 
-//   expressApp.get('/plugin-manifest.json', function (req, res) {
-//     res.sendfile('plugin-manifest.json');
-//   });
-
-//   expressApp.use('/app', express.static('app'));
-//   expressApp.use('/app', serveIndex('app'));
-
-
-//   expressApp.use('/', express.static('./'));
-
-//   var options = {
-//     key: fs.readFileSync('./key.pem'),
-//     cert: fs.readFileSync('./cert.pem')
-//   };
-
-//   https.createServer(options, expressApp).listen(port, function () {
-//     console.log(chalk.green('Zet running at ht' + 'tps://127.0.0.1:' + port));
-//     console.log(chalk.bold.cyan("Note: Please enable the host (https://127.0.0.1:" + port + ") in a new tab and authorize the connection by clicking Advanced->Proceed to 127.0.0.1 (unsafe)."));
-//   }).on('error', function (err) {
-//     if (err.code === 'EADDRINUSE') {
-//       console.log(chalk.bold.red(port + " port is already in use"));
-//     }
-//   });
-
-//   function validateDomains(domainsList) {
-//     var invalidURLs = domainsList.filter(function (domain) {
-//       return !isValidURL(domain);
-//     });
-
-//     return invalidURLs && invalidURLs.length > 0;
-//   }
-
-//   function isValidURL(url) {
-//     try {
-//       var parsedURL = new URL(url);
-//       if (parsedURL.protocol !== ('http' + ':') && parsedURL.protocol !== ('https' + ':') && parsedURL.protocol !== ('wss' + ':')) {
-//         return false;
-//       }
-//     } catch (e) { return false; }
-
-//     return true;
-//   }
-// });
-
+          // Gọi API ZOHO Creator để thêm record
+          ZOHO.CREATOR.API.addRecord(config)
+            .then(function (response) {
+              if (response.code == 3000) {
+                console.log("Record added successfully");
+              } else {
+                console.error("Error adding record:", response);
+              }
+            })
+            .catch(function (error) {
+              console.error("API call failed:", error);
+            });
+        } else {
+          console.error(
+            "Record does not contain 'data' key:",
+            record
+          );
+        }
+      });
+    } catch (e) {
+      console.error("Invalid JSON data", e);
+      alert("Please enter valid JSON data.");
+    }
+  }
+});
